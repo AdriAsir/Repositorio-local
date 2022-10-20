b78034a3d794e7cbf143c7e7a002b9790f3b4b95
AdriAsir <114144281+AdriAsir@users.noreply.github.com>
2022-10-20 22:52:50 +0200

    Maquinas, Vagrant y aprovisionamiento
    a

---
+++ script_front.sh
@@ -0,0 +1,6 @@
+sudo apt update 
+sudo apt upgrade 
+sudo apt get install -y apache2 
+sudo apt install -y php php-curl php-json php-gd php-zip php-mbstring libapache2-mod-php php-mysql
+sudo apt install -y wget
+
